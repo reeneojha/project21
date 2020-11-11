019f287f5a832d4dbf097bd0c10b27d5e3575289
reeneojha <73228861+reeneojha@users.noreply.github.com>
2020-11-11 18:30:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var bullet,wall;
+var thickness,speed,weight;
+
+function setup(){
+
+  createCanvas(1600,400);
+
+  thickness=random(32,60);
+
+  bullet=createSprite(20,200,50,50);
+  bullet.shapeColor="white";
+
+  speed=random(80,260);
+  
+  weight=random(30,50);
+  
+  bullet.velocityX=speed;
+  
+  wall=createSprite(1500,200,thickness,height/2);
+  wall.shapeColor="white";
+  
+}
+function draw(){
+  background("black");
+
+
+  drawSprites();
+
+  if(hasCollided(bullet,wall)){
+
+    bullet.velocityX=0;
+    
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    
+    if(damage>10){
+    
+      wall.shapeColor="red";
+    
+    }
+    
+    if(damage<10){
+    
+      wall.shapeColor="green";
+    
+    }
+ 
+  }
+}
+
+
+function hasCollided(bullet,wall){
+
+ // bulletRightEdge=bullet.x;
+  //wallLeftEdge=wall.x;
+
+  if(wall.x-bullet.x<wall.width/2+bullet.width/2){
+    return true;
+  }
+    {
+    return false;
+  }
+}
